1d171b495c27681482b7627d9faed1cb60275082
ExpertZs <zs.freelancer1@gmail.com>
2022-11-19 16:18:00 +0600

    Initializing Variables

--- src/com/expert_zs/Main.java
+++ src/com/expert_zs/Main.java
@@ -3,7 +3,13 @@ package com.expert_zs;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Print simple string
         System.out.println("Welcome back to Java world");
+    //Initializing variables
+        int age = 26;
+        int myAge =27;
+        int herAge = myAge-3;
+        System.out.println(herAge);
+
     }
 }
