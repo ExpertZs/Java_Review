bb6cd80958d64f8b1416091b3e0482087e66be27
ExpertZs <zs.freelancer1@gmail.com>
2022-11-19 16:36:39 +0600

    Initializing variables with primitive data type

--- src/com/expert_zs/Main.java
+++ src/com/expert_zs/Main.java
@@ -5,11 +5,20 @@ public class Main {
     public static void main(String[] args) {
 	// Print simple string
         System.out.println("Welcome back to Java world");
+
     //Initializing variables
-        int age = 26;
         int myAge =27;
         int herAge = myAge-3;
         System.out.println(herAge);
 
+        //Initializing variables with primitive data type
+        byte age = 26;
+        int number = 426_554_554;
+        long viewsCount = 3_426_554_554L;
+        float price = 55.4F;
+        double depositAmount = 55000.00;
+        char letter = 'A';
+        boolean isEligeble = true;
+        System.out.println(isEligeble);
     }
 }
